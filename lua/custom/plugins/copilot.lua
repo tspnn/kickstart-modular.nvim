d0943b3911e2f04c67d041b3c8fadc10df08e478
spikenn <420spn@gmail.com>
2024-03-12 10:23:51 +0500

    Replace github copilot with zbirenbaum copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,5 +1,36 @@
 return {
-    {
-        "github/copilot.vim",
+  {
+    'zbirenbaum/copilot.lua',
+    build = ':Copilot auth',
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = '<M-l>',
+          accept_word = false,
+          accept_line = false,
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = '[[',
+          jump_next = ']]',
+          accept = '<CR>',
+          refresh = 'gr',
+          open = '<M-CR>',
+        },
+        layout = {
+          position = 'bottom', -- | top | left | right
+          ratio = 0.4,
+        },
+      },
     },
+  },
 }
